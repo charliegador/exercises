00748a4949afb9e09a7aaa0f58246e188b1f0193
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-12 14:35:52 +0200

    Commit 6

---
+++ A-Beginner/6/script.js
@@ -0,0 +1,7 @@
+//6. Deklarera en variabel och tilldela den ett värde. Tilldela den sedan ett nytt värde på raden under.
+let name ="Charlie"
+console.log(name)
+
+name = "Ömer"
+
+console.log(name)
