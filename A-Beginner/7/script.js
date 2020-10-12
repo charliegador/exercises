ecccb8ee24162ede7b5781deded48eff40edd91f
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-12 14:49:05 +0200

    Commmit 7/2

---
+++ A-Beginner/7/script.js
@@ -0,0 +1,9 @@
+//7. Skapa en array med tre element
+// Gjorde två för llite träning 
+
+const students = ["Ömer", "Charlie", "Eddin"]
+
+const numbers = [251, 543, 892]
+
+console.log(numbers)
+console.log(students)
