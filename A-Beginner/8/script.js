4a3d73dbc1080162ceac8331a7b55189928b4ba0
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-17 16:27:39 +0200

    Commit träna igen

--- A-Beginner/8/script.js
+++ A-Beginner/8/script.js
@@ -1,5 +1,14 @@
 //8. Skapa ett objekt som representerar dig själv och som innehåller properties `firstName`, `lastName` och `age`
-let charlie = {height: "178cm", age: 16, lastName: "Gador", fingers: 10, toes:"10", smart: true, notSmart: false};
+let charlie = {
+    height: "178cm", 
+    age: 16, 
+    lastName: "Gador", 
+    fingers: 10, 
+    toes:"10", 
+    smart: true, 
+    notSmart: false,
+    movies: ["StarWars", "Pirates", "Harry Potter"]
+};
 let weight = 60.3;
 console.log(charlie);
 console.log(charlie.height);
@@ -7,3 +16,4 @@ console.log(charlie.age + charlie.fingers);
 console.log(charlie.age + charlie.toes);
 console.log(charlie.height + charlie.lastName);
 console.log(typeof (charlie.height + charlie.lastName));
+console.log(charlie.movies[1]);
