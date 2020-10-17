b9714d736ae5785d0efc1c85bd72c1a2fc36a4d8
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-17 17:13:57 +0200

    Commit 12 & några ändringar

--- A-Beginner/10/script.js
+++ A-Beginner/10/script.js
@@ -1,6 +1,7 @@
 //10. Konkatenera tre strängar
 console.log('hej ' + 'på ' + 'dig');
 
+//exprementering
 let hej = "då";
 console.log(hej.concat("re"));
 
---
+++ A-Beginner/12/script.js
@@ -0,0 +1,16 @@
+//12. Skriv en funktion och kalla på den
+function helloWorld(){
+    console.log("Välkommen till myFunction")
+    console.log("Hello World")
+};
+console.log("Före");
+helloWorld();
+console.log("Efter");
+
+//Experimentation
+function calcarea(base, height){
+    console.log("Välkommen till calcarea");
+    console.log(base * height);
+};
+calcarea(25, 10);
+calcarea(40, 5);
