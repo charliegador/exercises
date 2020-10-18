d3563b251d48b474368dc2802cf66da85097f3c4
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-10-18 18:05:16 +0200

    Uppgift B,2 igen

--- B-Web/2/script.js
+++ B-Web/2/script.js
@@ -1,4 +1,4 @@
 //2. Lägg till en paragraf med lite text under. Byt färg på paragrafen från JavaScript när du klickar på den.
 function changeColor(){
-    document.body.style.color = "blue";
+    document.getElementById("demo").style.color = "blue";
 }
